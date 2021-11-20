fa0bf155bb064425f6999d62c001bb96c18fc2d9
Hanna <sophianhanna@gmail.com>
2021-11-20 18:54:06 +0000

    formatted

--- src/components/toDoCard.tsx
+++ src/components/toDoCard.tsx
@@ -1,15 +1,12 @@
-import { toDoItemWithID } from "./toDoItemWithID"
+import { toDoItemWithID } from "./toDoItemWithID";
 
-export default function toDoCard(props: toDoItemWithID) : JSX.Element {
-    return (
-        <div className="toDoCard">
-             <h3>{props.title}</h3>
-             <p>{props.description}</p>
-             <small>{props.dueDate}</small>
-             <input
-                name="isCompleter"
-                type="checkbox"
-                checked={props.completed} />
-        </div>
-    )
+export default function toDoCard(props: toDoItemWithID): JSX.Element {
+  return (
+    <div className="toDoCard">
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+      <small>{props.dueDate}</small>
+      <input name="isCompleter" type="checkbox" checked={props.completed} />
+    </div>
+  );
 }
